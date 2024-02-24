60d00754a2fbc43c711a9c5abca1c4050350ff50
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-24 21:23:58 +0530

    Fetched plot and sky condition images

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place")
 days = st.slider("Forecast Days", 1, 5, 1, help="Select days to forecast")
 view = st.selectbox("Select Data to View", ('Temperature', 'Sky'))
-st.subheader(f"{view} for the next {days} days in {place}")
+
+if place:
+    st.subheader(f"{view} for the next {days} days in {place}")
+    data = backend.get_data(place, days)
+    dates = [item['dt_txt'] for item in data]
+    if view == "Temperature":
+        temperatures = [item['main']['temp']/10 for item in data]
+        figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+        st.plotly_chart(figure)
+    if view == "Sky":
+        sky = [item['weather'][0]['main'] for item in data]
+        imagespaths = ["images/"+item.lower()+".png" for item in sky]
+        st.image(imagespaths, width=100, caption=dates)
 
 
-data = backend.get_data(place,days,view)
 
 
-
-dates,temperatures = get_data(days)
-figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
-st.plotly_chart(figure)
