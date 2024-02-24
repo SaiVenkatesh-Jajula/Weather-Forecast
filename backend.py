2d8eccac90f698fecaadcbebe2b358c5e63ca271
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-24 20:07:33 +0530

    coded backend using API

---
+++ backend.py
@@ -0,0 +1,20 @@
+import requests
+
+API_KEY = "41ebe2232d376d644cec5b22de14328a"
+
+
+def get_data(place, days, view):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    # Data Processing!
+    filterdata = data['list'][:days * 8]
+    if view == 'Temperature':
+        temperatures = [item['main']['temp'] for item in filterdata]
+        return temperatures
+    if view == 'Sky':
+        sky = [item['weather'][0]['main'] for item in filterdata]
+        return sky
+
+if __name__ == "__main__":
+    print(get_data('Tokyo', 1, 'Sky'))
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import streamlit as st
 import plotly.express as px
+import backend
 
 st.title("Weather Forecast for the Next Days")
 
 place = st.text_input("Place")
 days = st.slider("Forecast Days", 1, 5, 1, help="Select days to forecast")
 view = st.selectbox("Select Data to View", ('Temperature', 'Sky'))
-
 st.subheader(f"{view} for the next {days} days in {place}")
 
 
-def get_data(days):
-    dates = ['1999-01-01', '1999-01-02', '1999-01-03']
-    temperatures = [49, 30, 29]
-    temperatures = [i*days for i in temperatures]
-    return dates, temperatures
+data = backend.get_data(place,days,view)
+
 
 
 dates,temperatures = get_data(days)
